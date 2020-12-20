83b5c4f57f1058a261ce34c3db60ee0585821794
Witik <ikbeneenkoekje@gmail.com>
2020-12-20 15:50:06 +0100

    day 10 pt 2

--- src/day10.js
+++ src/day10.js
@@ -5,9 +5,7 @@ const rawNumbers = buffer.toString().split('\n').filter(line => line !== '')
 
 const numbers = rawNumbers.map((raw) => +raw);
 
-const sum = (list) => list.reduce((a, b) => a+b, 0);
 const min = (list) => list.reduce((a, b) => Math.min(a,b), Number.MAX_VALUE);
-const max = (list) => list.reduce((a, b) => Math.max(a,b), Number.MIN_VALUE);
 
 (() => {
     console.log('part 1');
@@ -27,4 +25,42 @@ const max = (list) => list.reduce((a, b) => Math.max(a,b), Number.MIN_VALUE);
     }
 
     console.log(differences, differences[1] * differences[3]);
+})();
+
+(() => {
+    console.log('part 2');
+    const unused = [...numbers];
+    let jolts = 0;
+    const differences = [];
+
+    while (unused.length > 0) {
+        const lowest = min(unused);
+        unused.splice(unused.indexOf(lowest), 1);
+        const difference = lowest - jolts;
+        differences.push(difference);
+        jolts = lowest;
+    }
+    let ones = 0;
+    let possibilities = 1;
+    for (const difference of differences) {
+        if (difference === 1) {
+            ones++;
+        } else if (ones !== 0) {
+            if (ones === 4) {
+                possibilities *= 7
+            } else if (ones > 1) {
+                possibilities *= 2 ** ((ones - 1))
+            }
+            ones = 0;
+        }
+    }
+    if (ones !== 0) {
+        if (ones === 4) {
+            possibilities *= 7
+        } else if (ones > 1) {
+            possibilities *= 2 ** ((ones - 1))
+        }
+    }
+
+    console.log(possibilities);
 })();
